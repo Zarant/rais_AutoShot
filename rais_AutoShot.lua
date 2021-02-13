bfec587b5f104ab42eee2ededc7c1a01824234fe
Zarant <26726709+Zarant@users.noreply.github.com>
2021-02-13 18:25:19 -0300

    Added haste calculations for auto shot cast time

--- rais_AutoShot.lua
+++ rais_AutoShot.lua
@@ -18,7 +18,8 @@ end]]
 
 local Debug = false
 autoshot_latency = 0;
-local castTime = 0.50;
+local baseCastTime = 0.50;
+local castTime = baseCastTime
 --local AimedDelay = 0;
 local AutoRepeat = false
 
@@ -40,17 +41,14 @@ local swingTime
 local prevswing = 0
 local relative
 local InterruptTimer = 0
-
+local baseSpeed = 0
 
 
 local Lat
 
+rais_AutoShot = {}
 
-rais_AutoShot_Frame_Timer = nil
-rais_AutoShot_Frame_Timer2 = nil
-rais_AutoShot_Texture_Timer = nil
-rais_AutoShot_Texture_LATENCY = nil
-rais_AutoShot_Texture_BG = nil
+
 
 
 Table["posX"] = Table["posX"] *GetScreenWidth() /1000;
@@ -58,64 +56,67 @@ Table["posY"] = Table["posY"] *GetScreenHeight() /1000;
 Table["Width"] = Table["Width"] *GetScreenWidth() /1000;
 Table["Height"] = Table["Height"] *GetScreenHeight() /1000;
 
-rais_AutoShot_Frame_Timer = CreateFrame("Frame",nil,UIParent);
-local Frame = rais_AutoShot_Frame_Timer;
-Frame:SetFrameLevel(1)
-Frame:SetFrameStrata("HIGH");
-Frame:SetWidth(Table["Width"]);
-Frame:SetHeight(Table["Height"]);
-Frame:SetPoint("CENTER",UIParent,"CENTER",Table["posX"],Table["posY"]);
-Frame:SetAlpha(1);
 
-rais_AutoShot_Frame_Timer2 = CreateFrame("Frame",nil,UIParent);
-local Frame2 = rais_AutoShot_Frame_Timer2;
-Frame2:SetFrameLevel(2)
-Frame2:SetFrameStrata("HIGH");
-Frame2:SetWidth(Table["Width"]);
-Frame2:SetHeight(Table["Height"]);
-Frame2:SetPoint("CENTER",UIParent,"CENTER",Table["posX"],Table["posY"]);
-Frame2:SetAlpha(1);
+function rais_AutoShot.AutoShotBar_Create()
+	rais_AutoShot.Frame_Timer = CreateFrame("Frame",nil,UIParent);
+	local Frame = rais_AutoShot.Frame_Timer;
+	Frame:SetFrameLevel(1)
+	Frame:SetFrameStrata("HIGH");
+	Frame:SetWidth(Table["Width"]);
+	Frame:SetHeight(Table["Height"]);
+	Frame:SetPoint("CENTER",UIParent,"CENTER",Table["posX"],Table["posY"]);
+	Frame:SetAlpha(1);
 
-
-
-rais_AutoShot_Texture_Timer = Frame2:CreateTexture(nil,"OVERLAY"); --overlay
-local Bar = rais_AutoShot_Texture_Timer;
-Bar:SetHeight(Table["Height"]);
-Bar:SetTexture([[Interface\AddOns\rais_AutoShot\Textures\Bar.tga]]);
-Bar:SetPoint("CENTER",Frame2,"CENTER");
+	rais_AutoShot.Frame_Timer2 = CreateFrame("Frame",nil,UIParent);
+	local Frame2 = rais_AutoShot.Frame_Timer2;
+	Frame2:SetFrameLevel(2)
+	Frame2:SetFrameStrata("HIGH");
+	Frame2:SetWidth(Table["Width"]);
+	Frame2:SetHeight(Table["Height"]);
+	Frame2:SetPoint("CENTER",Frame,"CENTER");
+	Frame2:SetAlpha(1);
 
 
 
-rais_AutoShot_Texture_LATENCY = Frame:CreateTexture(nil,"OVERLAY");
-Lat = rais_AutoShot_Texture_LATENCY;
-Lat:SetHeight(Table["Height"]);
-Lat:SetTexture([[Interface\AddOns\rais_AutoShot\Textures\Bar.tga]]);
-Lat:SetPoint("CENTER",Frame,"CENTER");
-Lat:SetVertexColor(0.15,0.15,0.15)
-Lat:SetWidth(Table["Width"] * (castTime - castdelay)/castTime);
+	rais_AutoShot.Texture_Timer = Frame2:CreateTexture(nil,"OVERLAY"); --overlay
+	local Bar = rais_AutoShot.Texture_Timer;
+	Bar:SetHeight(Table["Height"]);
+	Bar:SetTexture([[Interface\AddOns\rais_AutoShot\Textures\Bar.tga]]);
+	Bar:SetPoint("CENTER",Frame2,"CENTER");
 
 
-rais_AutoShot_Texture_BG = Frame:CreateTexture(nil,"ARTWORK");
-Background = rais_AutoShot_Texture_BG;
-Background:SetHeight(Table["Height"]);
-Background:SetTexture([[Interface\AddOns\rais_AutoShot\Textures\Bar.tga]]);
-Background:SetPoint("CENTER",Frame,"CENTER");
-Background:SetVertexColor(0.5,0.5,0.5)
-Background:SetWidth(Table["Width"]);
+
+	rais_AutoShot.Texture_LATENCY = Frame:CreateTexture(nil,"OVERLAY");
+	Lat = rais_AutoShot.Texture_LATENCY;
+	Lat:SetHeight(Table["Height"]);
+	Lat:SetTexture([[Interface\AddOns\rais_AutoShot\Textures\Bar.tga]]);
+	Lat:SetPoint("CENTER",Frame,"CENTER");
+	Lat:SetVertexColor(0.15,0.15,0.15)
+	Lat:SetWidth(Table["Width"] * (castTime - castdelay)/castTime);
 
 
-Border = Frame:CreateTexture(nil,"BORDER"); 
-Border:SetPoint("CENTER",Frame,"CENTER");
-Border:SetWidth(Table["Width"] +2);
-Border:SetHeight(Table["Height"] +2);
-Border:SetColorTexture(0,0,0);
+	rais_AutoShot.Texture_BG = Frame:CreateTexture(nil,"ARTWORK");
+	Background = rais_AutoShot.Texture_BG;
+	Background:SetHeight(Table["Height"]);
+	Background:SetTexture([[Interface\AddOns\rais_AutoShot\Textures\Bar.tga]]);
+	Background:SetPoint("CENTER",Frame,"CENTER");
+	Background:SetVertexColor(0.5,0.5,0.5)
+	Background:SetWidth(Table["Width"]);
 
 
-local Border = Frame:CreateTexture(nil,"BACKGROUND");
-Border:SetPoint("CENTER",Frame,"CENTER");
-Border:SetWidth(Table["Width"] +4);
-Border:SetHeight(Table["Height"] +4);
-Border:SetColorTexture(1,1,1);
+	Border = Frame:CreateTexture(nil,"BORDER"); 
+	Border:SetPoint("CENTER",Frame,"CENTER");
+	Border:SetWidth(Table["Width"] +2);
+	Border:SetHeight(Table["Height"] +2);
+	Border:SetColorTexture(0,0,0);
+
+
+	local Border = Frame:CreateTexture(nil,"BACKGROUND");
+	Border:SetPoint("CENTER",Frame,"CENTER");
+	Border:SetWidth(Table["Width"] +4);
+	Border:SetHeight(Table["Height"] +4);
+	Border:SetColorTexture(1,1,1);
+end
 
 	
 local function autoshot_latency_update()
@@ -126,13 +127,21 @@ local function autoshot_latency_update()
 end
 
 local function SetBarAlpha(n)
-	rais_AutoShot_Frame_Timer:SetAlpha(n);
-	rais_AutoShot_Frame_Timer2:SetAlpha(n);
+	rais_AutoShot.Frame_Timer:SetAlpha(n);
+	rais_AutoShot.Frame_Timer2:SetAlpha(n);
 end
 
 
 local function Cast_Start()
 	
+	local haste
+	if baseSpeed == 0 then
+		haste = 1
+	else
+		haste = baseSpeed/UnitRangedDamage("player")
+	end
+	castTime = baseCastTime/haste
+	--print(castTime)
 	
 	SetBarAlpha(0)
 	if moving or IsSpellInRange(AutoName,"target") ~= 1 or not(AutoRepeat)then
@@ -142,7 +151,7 @@ local function Cast_Start()
 		SetBarAlpha(1)
 		--print(GetTime())
 		autoshot_latency_update()
-		rais_AutoShot_Texture_Timer:SetVertexColor(1,0,0);
+		rais_AutoShot.Texture_Timer:SetVertexColor(1,0,0);
 		castStart = GetTime();
 		if larg2 == nil then
 			larg2 = "nil"
@@ -181,11 +190,11 @@ local function Cast_Update()
 		--if  (UnitCastingInfo("player") ~= nil or IsCurrentSpell("Steady Shot") or IsCurrentSpell("Multi-Shot")) then --or InterruptTimer > GetTime()
 		--	Cast_Interrupted()
 	--	else
-			rais_AutoShot_Texture_Timer:SetWidth(Table["Width"] * relative/castTime);
+			rais_AutoShot.Texture_Timer:SetWidth(Table["Width"] * relative/castTime);
 	--	end
 	end
 	if ((relative > (castTime - castdelay)) and (castStart ~= false)) then
-		rais_AutoShot_Texture_Timer:SetVertexColor(0,0,0.5);		
+		rais_AutoShot.Texture_Timer:SetVertexColor(0,0,0.5);		
 
 	end
 	
@@ -208,7 +217,7 @@ local function Swing_Start(delay)
 	
 	--if (GetTime() - prevswing) > (prevswingspeed+0.3) then
 		SetBarAlpha(1)
-		rais_AutoShot_Texture_Timer:SetVertexColor(1,1,1);
+		rais_AutoShot.Texture_Timer:SetVertexColor(1,1,1);
 		castStart = false
 		swingStart = GetTime();
 		prevswing = swingStart;
@@ -223,6 +232,8 @@ end
 
 
 local Frame = CreateFrame("Frame");
+Frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+Frame:RegisterEvent("UNIT_INVENTORY_CHANGED")
 Frame:RegisterEvent("UNIT_SPELLCAST_SENT")
 Frame:RegisterEvent("PLAYER_LEVEL_UP")
 Frame:RegisterEvent("PLAYER_LOGIN")
@@ -265,6 +276,27 @@ end
 
 
 Frame:SetScript("OnEvent",function(self,event,arg1,arg2,arg3,arg4)
+	if Debug == true and event ~= nil then
+		lastevent = event
+		larg1 = arg1
+		larg2 = arg2
+		if not ((event == "WORLD_MAP_UPDATE") or (event == "UPDATE_SHAPESHIFT_FORM") or string.find(event,"LIST_UPDATE") or string.find(event,"COMBAT_LOG") or string.find(event,"CHAT") or string.find(event,"CHANNEL")) then
+			local a = GetTime()..' '..event..':'
+			if arg1 ~= nil then
+				a = a.."/"..tostring(arg1)
+			end
+			if arg2 ~= nil then
+				a = a.."/"..tostring(arg2)
+			end
+			if arg3 ~= nil then
+				a = a.."/"..tostring(arg3)
+			end
+			if arg4 ~= nil then
+				a = a.."/"..tostring(arg4)
+			end
+			DEFAULT_CHAT_FRAME:AddMessage(a)
+		end
+	end
 	
 	if event == "COMBAT_LOG_EVENT_UNFILTERED" then
 		timeStamp, event, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, spellID, spellName, x3, x4, x5 = CombatLogGetCurrentEventInfo()
@@ -298,61 +330,50 @@ Frame:SetScript("OnEvent",function(self,event,arg1,arg2,arg3,arg4)
 			end
 		end
 		return
-	end
-
-	if Debug == true and event ~= nil then
-		lastevent = event
-		larg1 = arg1
-		larg2 = arg2
-		if not ((event == "WORLD_MAP_UPDATE") or (event == "UPDATE_SHAPESHIFT_FORM") or string.find(event,"LIST_UPDATE") or string.find(event,"COMBAT_LOG") or string.find(event,"CHAT") or string.find(event,"CHANNEL")) then
-			local a = GetTime()..' '..event..':'
-			if arg1 ~= nil then
-				a = a.."/"..tostring(arg1)
-			end
-			if arg2 ~= nil then
-				a = a.."/"..tostring(arg2)
-			end
-			if arg3 ~= nil then
-				a = a.."/"..tostring(arg3)
-			end
-			if arg4 ~= nil then
-				a = a.."/"..tostring(arg4)
-			end
-			DEFAULT_CHAT_FRAME:AddMessage(a)
-		end
-	end
-	if event == "PLAYER_LEVEL_UP" then
+	elseif event == "PLAYER_LEVEL_UP" then
 		castdelay = autoshot_latency/1e3
 		autoshot_latency_update();
 		Swing_Start();
-	end
-	if ( event == "PLAYER_LOGIN" ) then
-		--AutoShotBar_Create();
+	elseif (event == "UNIT_INVENTORY_CHANGED" and arg1 == "player") or event == "PLAYER_ENTERING_WORLD" then
+		
+		GameTooltip:SetOwner(rais_AutoShot.Frame_Timer, "ANCHOR_RIGHT")
+		GameTooltip:SetInventoryItem("player",18)
+		
+		
+		for n = 2,GameTooltip:NumLines() do
+			local text = getglobal("GameTooltipTextRight"..n):GetText()
+			if text then
+				baseSpeed = tonumber(string.match(text,"%a+ (%d%p%d%d?)")) or 0
+				if baseSpeed > 0 then
+					break
+				end
+			end
+		end
+		
+		GameTooltip:Hide();
+		--print(baseSpeed)
+		
+	elseif ( event == "PLAYER_LOGIN" ) then
+		rais_AutoShot.AutoShotBar_Create();
 		DEFAULT_CHAT_FRAME:AddMessage("|cff00ccff"..AddOn.."|cffffffff Loaded");
-	end
-
-	if event == "PLAYER_STARTED_MOVING" then
+	elseif event == "PLAYER_STARTED_MOVING" then
 		moving = true
 		if swingStart == false then
 			Cast_Interrupted();	
 		end 
-	end
-	if event == "PLAYER_STOPPED_MOVING" then
+	elseif event == "PLAYER_STOPPED_MOVING" then
 		moving = false
 		--[[
-		if rais_AutoShot_Frame_Timer:GetAlpha() == 0 then
+		if rais_AutoShot.Frame_Timer:GetAlpha() == 0 then
 			Cast_Interrupted();	
 			Cast_Start()
 		end]]
-	end
-	
-	if event == "START_AUTOREPEAT_SPELL" then
+	elseif event == "START_AUTOREPEAT_SPELL" then
 		AutoRepeat = true
-		--[[if rais_AutoShot_Frame_Timer:GetAlpha() == 0 then
+		--[[if rais_AutoShot.Frame_Timer:GetAlpha() == 0 then
 			Cast_Interrupted()
 		end]]
-	end
-	if event == "STOP_AUTOREPEAT_SPELL" then
+	elseif event == "STOP_AUTOREPEAT_SPELL" then
 		AutoRepeat = false
 	end
 	
@@ -363,13 +384,8 @@ Frame:SetScript("OnEvent",function(self,event,arg1,arg2,arg3,arg4)
 		castdelay = autoshot_latency/1e3
 		autoshot_latency_update();
 		Swing_Start();
-	end
-	--[[if (event == "UNIT_SPELLCAST_FAILED_QUIET") and arg3 == AutoID then
-		ASfailed = GetTime()
---		Swing_Start()
-	end]]
-	--Resets auto shot timer after feign death 
-	if (event == "UNIT_AURA") then
+	elseif (event == "UNIT_AURA") then
+		--Resets auto shot timer after feign death 
 		local buffed = false
 		for i=1,32 do 
 			local name=UnitBuff("player",i); 
@@ -390,7 +406,7 @@ Frame:SetScript("OnEvent",function(self,event,arg1,arg2,arg3,arg4)
 	end
 
 	--[[ 
-	if rais_AutoShot_Frame_Timer:GetAlpha() == 0 and (arg3 == "Steady Shot" or arg3 == "Multi-Shot" or arg3 == "Aimed Shot") and (event == "UNIT_SPELLCAST_STOP") then
+	if rais_AutoShot.Frame_Timer:GetAlpha() == 0 and (arg3 == "Steady Shot" or arg3 == "Multi-Shot" or arg3 == "Aimed Shot") and (event == "UNIT_SPELLCAST_STOP") then
 		Cast_Interrupted();	
 	end
 	]]
@@ -431,11 +447,9 @@ Frame:SetScript("OnUpdate",function()
 
 		relative = GetTime() - swingStart
 		
-		rais_AutoShot_Texture_Timer:SetWidth(Table["Width"]*(1 - (relative/swingTime)));
-		rais_AutoShot_Texture_Timer:SetVertexColor(1,1,1);
+		rais_AutoShot.Texture_Timer:SetWidth(Table["Width"]*(1 - (relative/swingTime)));
+		rais_AutoShot.Texture_Timer:SetVertexColor(1,1,1);
 		
-
-	
 		if ( relative >= swingTime ) then
 			swingStart = false;
 				Cast_Interrupted()
